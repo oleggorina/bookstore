116bd8e3bf6359dc582c731924e1dfa5a4145669
oleggorina <oleg.gorina@gmail.com>
2023-06-20 12:05:18 +0300

    Add adaptive to the about page

--- src/app/pages/about/components/about-author/about-author.component.ts
+++ src/app/pages/about/components/about-author/about-author.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faFacebookF, faTwitter, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
 
 @Component({
   selector: 'app-about-author',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-author.component.scss']
 })
 export class AboutAuthorComponent {
-
+  facebook = faFacebookF;
+  twitter = faTwitter;
+  linkedin = faLinkedinIn;
 }
